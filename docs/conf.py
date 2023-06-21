ec8b65b8ba1c5883b51fea6c7614120284cce694
Krishna-13-cyber <krishnanarayanan132002@gmail.com>
2023-06-21 11:50:54 +0530

    Update with conf.py changes

--- docs/conf.py
+++ docs/conf.py
@@ -27,13 +27,34 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 html_theme = 'alabaster'
 html_static_path = ['_static']
 
-INTEROP_ROOT = '..'
+html_theme_options = {
+    "github_user": "vgvassilev",
+    "github_repo": "CppInterOp",
+    "github_banner": True,
+    "fixed_sidebar": True,
+}
+
+highlight_language = "C++"
+
+todo_include_todos = True
+
+mathjax_path = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"
+
+# Add latex physics package
+mathjax3_config = {
+    "loader": {"load": ["[tex]/physics"]},
+    "tex": {"packages": {"[+]": ["physics"]}},
+}
+import os
+
+current_file_dir = os.path.dirname(os.path.realpath(__file__))
+INTEROP_ROOT = '..' + current_file_dir
 
 # html_extra_path = [INTEROP_ROOT + '/build/docs/doxygen/html']
 
 import subprocess
-command = 'mkdir {0}/build; cd {0}/build; cmake ../ -DClang_DIR=/usr/lib/llvm-14/lib/cmake/clang\
-         -DLLVM_DIR=/usr/lib/llvm-14 -DINTEROP_ENABLE_DOXYGEN=ON\
+command = 'mkdir {0}/build; cd {0}/build; cmake ../ -DClang_DIR=/usr/lib/llvm-10/lib/cmake/clang\
+         -DLLVM_DIR=/usr/lib/llvm-10 -DINTEROP_ENABLE_DOXYGEN=ON\
          -DINTEROP_INCLUDE_DOCS=ON'.format(INTEROP_ROOT)
 subprocess.call(command, shell=True)
 subprocess.call('doxygen {0}/build/docs/doxygen.cfg'.format(INTEROP_ROOT), shell=True)
